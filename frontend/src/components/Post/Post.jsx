cbfbec9121ba3606f6861ee54c7202e09d143b36
takesan923 <kannazuki3410@gmail.com>
2025-05-15 21:31:07 +0900

    いいね加算実装

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,39 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Post.css";
 import { MoreVert } from '@mui/icons-material';
 import { Users } from '../../DummyData'
 
 
 export default function Post({ post }) {
-    // const user = Users.filter((user) => user.id == 1)
-    // console.log(user[0].username);
-  return (
-    <div className='post'>
-        <div className="postWrapper">
-            <div className="postTop">
-                <div className="postTopLeft">
-                    <img src= {Users.filter((user) => user.id === post.id)[0].profilePicture} alt="" className="postProfileImg" />
-                    <span className="postUsername">{Users.filter((user) => user.id === post.id)[0].username}</span>
-                    <span className="postDate">{post.date}</span>
+    const [like, setlike] = useState(post.like);
+    const [isliked, setIslike] = useState(false);
+
+    const handlelile = () => {
+        setlike(isliked ? like - 1 : like + 1);
+        setIslike(!isliked);
+    }
+
+    return (
+        <div className='post'>
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img src= {Users.filter((user) => user.id === post.id)[0].profilePicture} alt="" className="postProfileImg" />
+                        <span className="postUsername">{Users.filter((user) => user.id === post.id)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert />
+                    </div>
                 </div>
-                <div className="postTopRight">
-                    <MoreVert />
+                <div className="postCenter">
+                    <span className="postText">{post.desc}</span>
+                    <img src= {post.photo} alt="" className="postImg" />
                 </div>
-            </div>
-            <div className="postCenter">
-                <span className="postText">{post.desc}</span>
-                <img src= {post.photo} alt="" className="postImg" />
-            </div>
-            <div className="postButtom">
-                <div className="postButtomLeft">
-                    <img src="./assets/heart.png" alt="" className="likeIcon" />
-                    <span className="postLikeCounter"> {post.like}人がいいねを押しました</span>
-                </div>
-                <div className="postButtomRight">
-                    <span className="postCommentText">{post.comment}:コメント</span>
+                <div className="postButtom">
+                    <div className="postButtomLeft">
+                        <img 
+                            src="./assets/heart.png" 
+                            alt="" className="likeIcon"  
+                            onClick={() => handlelile()}
+                        />
+                        <span className="postLikeCounter"> {like}人がいいねを押しました</span>
+                    </div>
+                    <div className="postButtomRight">
+                        <span className="postCommentText">{post.comment}:コメント</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
